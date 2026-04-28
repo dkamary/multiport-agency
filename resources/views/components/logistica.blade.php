a55d468a950a561b8ab79836082d56f0cadc9d85
Donatien Kamary <donatkamary@gmail.com>
2026-04-28 13:55:02 +0400

    Google Analytics

--- resources/views/components/logistica.blade.php
+++ resources/views/components/logistica.blade.php
@@ -68,6 +68,17 @@
     @if (!empty($stylesheets))
         {!! $stylesheets !!}
     @endif
+
+    {{-- Google tag (gtag.js) --}}
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-S6NHBVRS02"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-S6NHBVRS02');
+    </script>
+
     
 </head>
 <body {{ $attributes->merge(['class' => 'multiport-agency-v2']) }}>
